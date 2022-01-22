3573bae490ffebe3b337c54d1a840073decde7d6
Nguyễn Thành Luân <ntluan19@clc.fitus.edu.vn>
2022-01-22 19:25:53 +0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def main():
-    print("Mạng máy tính nâng cao")
-    print("19127443 - Hồ Đăng Khoa")
-    print("19127467 - Nguyễn Thành Luân")
+    print("Advance Computer Network")
+    print("19127443 - Ho Dang Khoa")
+    print("19127467 - Nguyen Thanh Luan")
 
 main()
