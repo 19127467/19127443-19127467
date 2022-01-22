44029faaca4d4a950780d950bf3590bbd87731f9
Nguyễn Thành Luân <ntluan19@clc.fitus.edu.vn>
2022-01-22 18:31:56 +0700

    Second commit

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from typing import no_type_check
-from flask import Flask, redirect, render_template,request
-app = Flask(__name__)
-
-@app.route('/')
-
-def home():
-    return render_template("index.html")
-if __name__ == "__main__":
-    app.run(debug=True)
+import os
+import webbrowser
+filename = 'file://'+os.getcwd()+'/' + 'templates/index.html'
+webbrowser.open_new(filename)
+webbrowser.open(filename)
